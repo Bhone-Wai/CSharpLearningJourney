4dfd8e341ddf67cc075009d56484b1a9069e743b
Bhone Wai <damenaosan@gmail.com>
2025-08-12 23:47:26 +0700

    Add StringCharsDemo class demonstrating basic string and char usage

---
+++ CSharpLearningJourney/StringChars/StringCharsDemo.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharpLearningJourney.StringChars
+{
+	public class StringCharsDemo
+	{
+		public static void Run()
+		{
+            string name = "";
+            Char letter = '\0';
+
+            Console.Write("My name is ");
+            Console.Write(name);
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+	}
+}
+
