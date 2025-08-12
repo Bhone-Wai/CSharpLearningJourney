3b09fa5dec51b3c40a2677de76cf9e9d25b1d045
Bhone Wai <damenaosan@gmail.com>
2025-08-12 23:44:34 +0700

    Add BooleanDemo class demonstrating basic boolean usage and output

--- CSharpLearningJourney/Boolean/BooleanDemo.cs
+++ CSharpLearningJourney/Boolean/BooleanDemo.cs
@@ -2,5 +2,12 @@ namespace CSharpLearningJourney.Boolean;
 
 public class BooleanDemo
 {
-    
+    public static void Run()
+    {
+        bool isLoading = true;
+        Console.WriteLine(isLoading);
+
+        isLoading = false;
+        Console.WriteLine(isLoading);
+    }
 }
