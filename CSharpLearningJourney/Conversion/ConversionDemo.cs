a986ce53ae06aeb32e624da7801bbfb0077fe4a0
Bhone Wai <damenaosan@gmail.com>
2025-08-12 23:43:25 +0700

    Add ConversionDemo class demonstrating string to numeric type conversions

--- CSharpLearningJourney/Conversion/ConversionDemo.cs
+++ CSharpLearningJourney/Conversion/ConversionDemo.cs
@@ -2,5 +2,26 @@ namespace CSharpLearningJourney.Conversion;
 
 public class ConversionDemo
 {
-    
+    public static void Run()
+    {
+        string textAge = "-22";
+        int age = Convert.ToInt32(textAge);
+        Console.WriteLine(age);
+
+        string textBigNumber = "-9000000";
+        long bigNumber = Convert.ToInt64(textBigNumber);
+        Console.WriteLine(bigNumber);
+
+        string textNegative = "-55.2";
+        double negative = Convert.ToDouble(textNegative);
+        Console.WriteLine(negative);
+
+        string textPercision = "5.001";
+        float percision = Convert.ToSingle(textPercision);
+        Console.WriteLine(percision);
+
+        string textMoney = "69.69";
+        decimal money = Convert.ToDecimal(textMoney);
+        Console.WriteLine(money);
+    }
 }
