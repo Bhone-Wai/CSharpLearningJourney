0d1b81e4e8e01800b0b46157c4565a57b1a84646
Bhone Wai <damenaosan@gmail.com>
2025-08-13 20:25:05 +0700

    Add VarDemo class demonstrating basic var keyword usage

---
+++ CSharpLearningJourney/Var/VarDemo.cs
@@ -0,0 +1,28 @@
+namespace CSharpLearningJourney.Var;
+
+public class VarDemo
+{
+    public static void Run()
+    {
+        var age = 22;
+        Console.WriteLine(age);
+
+        var bigNumber = -900000L;
+        Console.WriteLine(bigNumber);
+
+        var negative = -55.2D;
+        Console.WriteLine(negative);
+
+        var percision = 5.0001F;
+        Console.WriteLine(percision);
+        
+        var money = 69.00M;
+        Console.WriteLine(money);
+
+        var name = "Bhone Wai";
+        Console.WriteLine(name);
+        
+        var letter = 'b';
+        Console.WriteLine(letter);
+    }
+}
