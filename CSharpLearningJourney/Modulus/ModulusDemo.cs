0ff0fb3781d4922b9725d594808760be87d9fecd
Bhone Wai <damenaosan@gmail.com>
2025-08-13 20:24:51 +0700

    Add ModulusDemo class demonstrating basic modulus operator usage

---
+++ CSharpLearningJourney/Modulus/ModulusDemo.cs
@@ -0,0 +1,12 @@
+namespace CSharpLearningJourney.Modulus;
+
+public class ModulusDemo
+{
+    public static void Run()
+    {
+        int firstNum  = 10;
+        int secondNum = 3;
+
+        Console.WriteLine(firstNum % secondNum);
+    }
+}
