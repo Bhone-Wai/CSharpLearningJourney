f05f99c6c7cbac38281f92f2bacef80bf4592679
Bhone Wai <damenaosan@gmail.com>
2025-08-14 23:24:05 +0700

    Add StringFormattingDemo class demonstrating basic string formatting

--- CSharpLearningJourney/StringFormatting/StringFormattingDemo.cs
+++ CSharpLearningJourney/StringFormatting/StringFormattingDemo.cs
@@ -2,5 +2,19 @@ namespace CSharpLearningJourney.StringFormatting;
 
 public class StringFormattingDemo
 {
-    
+    public static void Run()
+    {
+        string name = "Kuzu";
+        int age = 22;
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Name: " + name + "\nAge: " + age);
+
+        Console.WriteLine("My name is {0} and my age is {1}", name, age);
+        Console.WriteLine("Name: {0}\nAge: {1}", name, age);
+    }
 }
