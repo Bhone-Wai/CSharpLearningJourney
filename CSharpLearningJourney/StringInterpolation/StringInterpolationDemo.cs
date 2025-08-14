79adba0f2dc17781d1933f48c9694914926e1962
Bhone Wai <damenaosan@gmail.com>
2025-08-14 23:25:35 +0700

    Add StringInterpolationDemo class demonstrating basic string interpolation

--- CSharpLearningJourney/StringInterpolation/StringInterpolationDemo.cs
+++ CSharpLearningJourney/StringInterpolation/StringInterpolationDemo.cs
@@ -1,9 +1,15 @@
 namespace CSharpLearningJourney.StringInterpolation;
 
-public class StringInterpolation
+public class StringInterpolationDemo
 {
     public static void Run()
     {
+        string name = "Kuzu";
+        int age = 22;
+
+        Console.WriteLine($"Your name is {name} and age is {age}");
         
+        string test = string.Concat("Your name is ", name, " and age is ", age);
+        Console.WriteLine(test);
     }
 }
