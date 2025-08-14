d161244e5fd755d373f6844f9f3a09afb9a4716a
Bhone Wai <damenaosan@gmail.com>
2025-08-14 23:27:18 +0700

    Add VerbatimStringLiteralDemo class demonstrating basic verbatim string literal usage

--- CSharpLearningJourney/VerbatimStringLiteral/VerbatimStringLiteralDemo.cs
+++ CSharpLearningJourney/VerbatimStringLiteral/VerbatimStringLiteralDemo.cs
@@ -2,5 +2,17 @@ namespace CSharpLearningJourney.VerbatimStringLiteral;
 
 public class VerbatimStringLiteralDemo
 {
-    
+    public static void Run()
+    {
+        string speech = "He is \"yapping about something\"";
+        string path = "Local HD:\\Users\\appleclub\\Documents\\Practice 2025\\C#\nNew line test";
+        Console.WriteLine(speech);
+        Console.WriteLine(path);
+
+        path = @"Local HD:/Users/appleclub/Documents/Practice 2025/C#" + "\nNew line test";
+        Console.WriteLine(path);
+
+        string name = @"Hello ""Kuzu""";
+        Console.WriteLine(name);
+    }
 }
