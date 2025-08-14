13e61f2339fd10ed2adca589eafc13159328a211
Bhone Wai <damenaosan@gmail.com>
2025-08-14 23:19:35 +0700

    Add TimesTableDemo class demonstrating basic times table generation

--- CSharpLearningJourney/ExerciseTimesTable/TimesTableDemo.cs
+++ CSharpLearningJourney/ExerciseTimesTable/TimesTableDemo.cs
@@ -1,6 +1,15 @@
 namespace CSharpLearningJourney.ExerciseTimesTable;
 
-public class TimesTable
+public class TimesTableDemo
 {
-    
+    public static void Run()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("{0} x {1} = {2} ", i, number, i * number);
+        }
+    }
 }
