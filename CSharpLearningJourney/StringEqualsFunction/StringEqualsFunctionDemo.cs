d2840f62a30dbbf76fc5325f2480f1a04cac3749
Bhone Wai <damenaosan@gmail.com>
2025-08-14 23:22:37 +0700

    Add StringEqualsFunctionDemo class demonstrating basic string comparison

--- CSharpLearningJourney/StringEqualsFunction/StringEqualsFunctionDemo.cs
+++ CSharpLearningJourney/StringEqualsFunction/StringEqualsFunctionDemo.cs
@@ -2,5 +2,42 @@ namespace CSharpLearningJourney.StringEqualsFunction;
 
 public class StringEqualsFunctionDemo
 {
-    
+    public static void Run()
+    {
+        string message = "Hello";
+        string compare = "Hello";
+
+        if (message == compare)
+        {
+            Console.WriteLine("Same");
+        }
+        else
+        {
+            Console.WriteLine("Different");
+        }
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        
+        if (!name.Equals(""))
+        {
+            Console.WriteLine($"Your name is {name}");
+        }
+        else
+        {
+            Console.WriteLine("Invalidate name input");
+        }
+        
+        char[] chars = new char[] { 'H', 'e', 'l', 'l', 'o' };
+        object newCompare = new string(chars);
+
+        if (message.Equals(newCompare))
+        {
+            Console.WriteLine("Same");
+        }
+        else
+        {
+            Console.WriteLine("Different");
+        }
+    }
 }
