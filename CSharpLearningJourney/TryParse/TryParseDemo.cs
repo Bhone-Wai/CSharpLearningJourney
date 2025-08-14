78ad6b6bfd45ae32900a7053aa8c7e56ce5e0bc2
Bhone Wai <damenaosan@gmail.com>
2025-08-14 23:26:38 +0700

    Add TryParseDemo class demonstrating basic TryParse usage

--- CSharpLearningJourney/TryParse/TryParseDemo.cs
+++ CSharpLearningJourney/TryParse/TryParseDemo.cs
@@ -2,5 +2,24 @@ namespace CSharpLearningJourney.TryParse;
 
 public class TryParseDemo
 {
-    
+    public static void Run()
+    {
+        bool success = true;
+
+        while (success)
+        {
+            Console.Write("Enter a number: ");
+            string numInput = Console.ReadLine();
+
+            if (int.TryParse(numInput, out int num))
+            {
+                success = false;
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine("Failed to convert number.");
+            }
+        }
+    }
 }
