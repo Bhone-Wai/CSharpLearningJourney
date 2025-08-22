4399a0474d715a55169fb0172958527a81555e1f
Bhone Wai <damenaosan@gmail.com>
2025-08-22 22:58:34 +0700

    Add NumberSplitDemo class demonstrating how to split numbers into odd and even lists.

--- CSharpLearningJourney/NumberSplit/NumberSplitDemo.cs
+++ CSharpLearningJourney/NumberSplit/NumberSplitDemo.cs
@@ -2,5 +2,33 @@ namespace CSharpLearningJourney.NumberSplit;
 
 public class NumberSplitDemo
 {
-    
+    public static void Run()
+    {
+        List<int> odd = new List<int>();
+        List<int> even = new List<int>();
+
+        for (int i = 0; i <= 20; i++)
+        {
+            if (i % 2 == 0)
+            {
+                even.Add(i);
+            }
+            else
+            {
+                odd.Add(i);
+            }
+        }
+
+        foreach (var num in even)
+        {
+            Console.Write($"{num} ");
+        }
+
+        Console.WriteLine();
+        
+        foreach (var num in odd)
+        {
+            Console.Write($"{num} ");
+        }
+    }
 }
