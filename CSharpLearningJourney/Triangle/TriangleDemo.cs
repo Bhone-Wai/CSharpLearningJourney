f06630c26f8e379d7e53206e64b0073417d40a5d
Bhone Wai <damenaosan@gmail.com>
2025-08-22 23:01:27 +0700

    Add TriangleDemo class demonstrating how to check if a triangle is valid based on its angles.

--- CSharpLearningJourney/Triangle/TriangleDemo.cs
+++ CSharpLearningJourney/Triangle/TriangleDemo.cs
@@ -2,5 +2,35 @@ namespace CSharpLearningJourney.Triangle;
 
 public class TriangleDemo
 {
-    
+    public static void Run()
+    {
+        /*const int angleCount = 3;
+        int[] angles = new int[angleCount];
+
+        for (int i = 0; i < angles.Length; i++)
+        {
+            Console.Write($"Enter angle {i + 1}: ");
+            angles[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int angleSum = 0;
+
+        foreach (int angle in angles)
+        {
+            angleSum += angle;
+        }
+        // Console.WriteLine($"Sum of angles: {angleSum}");
+
+        Console.WriteLine(angleSum == 180 ? "valid" :  "invalid");*/
+
+        int angleCount = 3;
+        int angleSum = 0;
+        for (int i = 0; i < angleCount; i++)
+        {
+            Console.Write($"Enter angle {i + 1}: ");
+            angleSum += Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine(angleSum == 180 ? "valid" : "invalid");
+    }
 }
