b05804db637db554b60f873f7615331c61c1f0b1
Bhone Wai <damenaosan@gmail.com>
2025-08-22 22:53:33 +0700

    feat: Add Dictionary and List examples

--- CSharpLearningJourney/Dictionary/DictionaryDemo.cs
+++ CSharpLearningJourney/Dictionary/DictionaryDemo.cs
@@ -1,6 +1,58 @@
+using System.Globalization;
+
 namespace CSharpLearningJourney.Dictionary;
+using System.Collections.Generic;
 
 public class DictionaryDemo
 {
-    
+    public static void Run()
+    {
+        Dictionary<int, string> names = new Dictionary<int, string>()
+        {
+            { 1, "Katsu" },
+            { 4, "Katsu" },
+            { 2, "o-san" },
+            { 3, "Noriko" }
+        };
+
+        /*for (int i = 0; i < names.Count; i++)
+        {
+            KeyValuePair<int, string> pair = names.ElementAt(i);
+            Console.WriteLine($"{pair.Key} - {pair.Value}");
+        }*/
+
+        /*foreach (KeyValuePair<int, string> pair in names)
+        {
+            Console.WriteLine($"{pair.Key} - {pair.Value}");
+        }*/
+        
+        /*names.Add(1, "Katsu");
+        names.Add(2, "o-san");
+        names.Add(3, "Noriko");*/
+
+        Dictionary<string, string> teachers = new Dictionary<string, string>()
+        {
+            { "Meth", "Bhone Wai" },
+            { "English", "Noriko" }
+        };
+
+        // Console.WriteLine(teachers["meth"]);
+        if (teachers.TryGetValue("Meth", out string teacher))
+        {
+            Console.WriteLine(teacher);
+
+            teachers["Meth"] = "Kuzu";
+        }
+        else
+        {
+            Console.WriteLine("Not found");
+        }
+        
+        teachers.Remove("Meth");
+
+        foreach (var pair in teachers)
+        {
+            Console.WriteLine($"{pair.Key}: {pair.Value}");
+        }
+    }
 }
--- CSharpLearningJourney/Lists/ListsDemo.cs
+++ CSharpLearningJourney/Lists/ListsDemo.cs
@@ -2,5 +2,29 @@ namespace CSharpLearningJourney.Lists;
 
 public class ListsDemo
 {
-    
+    public static void Run()
+    {
+        int[] numbers = new int[3]
+        {
+            1, 2, 3
+        };
+
+        List<int> listNumbers = new List<int>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Enter the number: ");
+            listNumbers.Add(Convert.ToInt32(Console.ReadLine()));
+        }
+
+        for (int i = 0; i < listNumbers.Count; i++)
+        {   
+            Console.Write($"{listNumbers[i]} ");
+        }
+
+        /*foreach (var num in listNumbers)
+        {
+            Console.Write($"{num} ");
+        }*/
+    }
 }
