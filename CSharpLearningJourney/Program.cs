f82a5fa429e3b32a6fa7e359aaf6b96d5bda2f29
Bhone Wai <damenaosan@gmail.com>
2025-08-22 23:02:17 +0700

    Update Program.cs to include new demo classes in the main menu.

--- CSharpLearningJourney/Program.cs
+++ CSharpLearningJourney/Program.cs
@@ -1,4 +1,10 @@
 using System;
+using CSharpLearningJourney.Array;
+using CSharpLearningJourney.ArrayClearing;
+using CSharpLearningJourney.ArrayIndexOf;
+using CSharpLearningJourney.ArrayOfMultiples;
+using CSharpLearningJourney.ArrayReversal;
+using CSharpLearningJourney.ArraySorting;
 using CSharpLearningJourney.DataTypes;
 using CSharpLearningJourney.StringChars;
 using CSharpLearningJourney.Conversion;
@@ -6,20 +12,31 @@ using CSharpLearningJourney.Boolean;
 using CSharpLearningJourney.ConditionalOperator;
 using CSharpLearningJourney.ConsoleIO;
 using CSharpLearningJourney.Constants;
+using CSharpLearningJourney.Dictionary;
 using CSharpLearningJourney.EmptyString;
 using CSharpLearningJourney.Exercise;
 using CSharpLearningJourney.For;
 using CSharpLearningJourney.Format;
+using CSharpLearningJourney.Functions;
 using CSharpLearningJourney.If;
+using CSharpLearningJourney.IsNullOrEmpty;
+using CSharpLearningJourney.Lists;
 using CSharpLearningJourney.Modulus;
+using CSharpLearningJourney.NumberSplit;
 using CSharpLearningJourney.Operations;
+using CSharpLearningJourney.PasswordChecker;
+using CSharpLearningJourney.PrintStringInReverse;
+using CSharpLearningJourney.ReturnTypeFunctions;
 using CSharpLearningJourney.StringEqualsFunction;
 using CSharpLearningJourney.StringFormatting;
 using CSharpLearningJourney.StringInterpolation;
+using CSharpLearningJourney.StringIteration;
 using CSharpLearningJourney.Switch;
+using CSharpLearningJourney.Triangle;
 using CSharpLearningJourney.TryParse;
 using CSharpLearningJourney.Var;
 using CSharpLearningJourney.VerbatimStringLiteral;
+using CSharpLearningJourney.VoidFunctions;
 using CSharpLearningJourney.While;
 
 namespace CSharpLearningJourney
@@ -54,6 +71,23 @@ namespace CSharpLearningJourney
             Console.WriteLine("22 String Interpolation Demo");
             Console.WriteLine("23 Empty String Demo");
             Console.WriteLine("24 String Equals Demo");
+            Console.WriteLine("25 String Iteration Demo");
+            Console.WriteLine("26 Is Null Or Empty Demo");
+            Console.WriteLine("27 Print String In Reverse Demo");
+            Console.WriteLine("28 Password Checker Demo");
+            Console.WriteLine("29 Array Demo");
+            Console.WriteLine("30 Triangle Demo");
+            Console.WriteLine("31 Array Sorting Demo");
+            Console.WriteLine("32 Array Reversal Demo");
+            Console.WriteLine("33 Array Clearing Demo");
+            Console.WriteLine("34 Array Index Of Demo");
+            Console.WriteLine("35 Lists Demo");
+            Console.WriteLine("36 Dictionary Demo");
+            Console.WriteLine("37 Number Split Demo");
+            Console.WriteLine("38 Array Of Multiples Demo");
+            Console.WriteLine("39 Functions Demo");
+            Console.WriteLine("40 Void Functions Demo");
+            Console.WriteLine("41 Return Types Functions Demo");
             Console.WriteLine("0. Exit");
             Console.Write("Enter your choice: ");
 
@@ -156,6 +190,74 @@ namespace CSharpLearningJourney
                 case "24":
                     Console.WriteLine("\n--- Running String Equals Demo ---");
                     StringEqualsFunctionDemo.Run();
+                    break;
+                case "25":
+                    Console.WriteLine("\n--- Running String Iteration Demo ---");
+                    StringIterationDemo.Run();
+                    break;
+                case "26":
+                    Console.WriteLine("\n--- Running Is Null Or Empty Demo ---");
+                    IsNullOrEmptyDemo.Run();
+                    break;
+                case "27":
+                    Console.WriteLine("\n--- Running Print String In Reverse Demo ---");
+                    PrintStringInReverseDemo.Run();
+                    break;
+                case "28":
+                    Console.WriteLine("\n--- Running Password Checker Demo ---");
+                    PasswordCheckerDemo.Run();
+                    break;
+                case "29":
+                    Console.WriteLine("\n--- Running Array Demo ---");
+                    ArrayDemo.Run();
+                    break;
+                case "30":
+                    Console.WriteLine("\n--- Running Triangle Demo ---");
+                    TriangleDemo.Run();
+                    break;
+                case "31":
+                    Console.WriteLine("\n--- Running Array Sorting Demo ---");
+                    ArraySortingDemo.Run();
+                    break;
+                case "32":
+                    Console.WriteLine("\n--- Running Array Reversal Demo ---");
+                    ArrayReversalDemo.Run();
+                    break;
+                case "33":
+                    Console.WriteLine("\n--- Running Array Clearing Demo ---");
+                    ArrayClearingDemo.Run();
+                    break;
+                case "34":
+                    Console.WriteLine("\n--- Running Array Index Of Demo ---");
+                    ArrayIndexOfDemo.Run();
+                    break;
+                case "35":
+                    Console.WriteLine("\n--- Running Lists Demo ---");
+                    ListsDemo.Run();
+                    break;
+                case "36":
+                    Console.WriteLine("\n--- Running Dictionary Demo ---");
+                    DictionaryDemo.Run();
+                    break;
+                case "37":
+                    Console.WriteLine("\n--- Running Number Split Demo ---");
+                    NumberSplitDemo.Run();
+                    break;
+                case "38":
+                    Console.WriteLine("\n--- Running Array Of Multiples Demo ---");
+                    ArrayOfMultiplesDemo.Run();
+                    break;
+                case "39":
+                    Console.WriteLine("\n--- Running Functions Demo ---");
+                    FunctionsDemo.Run();
+                    break;
+                case "40":
+                    Console.WriteLine("\n--- Running Void Functions Demo ---");
+                    VoidFunctionsDemo.Run();
+                    break;
+                case "41":
+                    Console.WriteLine("\n--- Running Return Types Functions Demo ---");
+                    ReturnTypeFunctionsDemo.Run();
                     break;
                 case "0":
                     Console.WriteLine("Goodbye!");
