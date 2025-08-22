2bc30088d2d9a20462bdb2d9aa684a7aa7f2d202
Bhone Wai <damenaosan@gmail.com>
2025-08-22 23:00:13 +0700

    Add PasswordCheckerDemo class demonstrating a simple password validation and confirmation.

--- CSharpLearningJourney/PasswordChecker/PasswordCheckerDemo.cs
+++ CSharpLearningJourney/PasswordChecker/PasswordCheckerDemo.cs
@@ -2,5 +2,45 @@ namespace CSharpLearningJourney.PasswordChecker;
 
 public class PasswordCheckerDemo
 {
-    
+    public static void Run()
+    {
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+        
+        Console.Write("Enter password again: ");
+        string passwordC = Console.ReadLine();
+
+        Console.WriteLine(password);
+        Console.WriteLine(passwordC);
+
+        if (!password.Equals(string.Empty))
+        {
+            if (!passwordC.Equals(string.Empty))
+            {
+                if (password.Length >= 6 && passwordC.Length >= 6)
+                {
+                    if (password.Equals(passwordC))
+                    {
+                        Console.WriteLine("Passwords are matched");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Passwords are not matched");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter 6 or more characters!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter password confirmation");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter password");
+        }
+    }
 }
